2adc0ed899f44a8a63458cd036b8321de31201b6
chrisamico <eyeseast@gmail.com>
2012-10-30 23:12:57 -0400

    Map assets

---
+++ js/wax/control/lib/location.js
@@ -0,0 +1,27 @@
+var wax = wax || {};
+
+wax.location = function() {
+
+    var t = {};
+
+    function on(o) {
+        console.log(o);
+        if ((o.e.type === 'mousemove' || !o.e.type)) {
+            return;
+        } else {
+            var loc = o.formatter({ format: 'location' }, o.data);
+            if (loc) {
+                window.location.href = loc;
+            }
+        }
+    }
+
+    t.events = function() {
+        return {
+            on: on
+        };
+    };
+
+    return t;
+
+};
